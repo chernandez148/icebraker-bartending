740f8722f52b34c33af66db852f536a80e8ff90d
Christian <hernandezchristian94@gmail.com>
2023-09-17 14:09:10 -0700

    updated json

--- src/components/Home/Hero/Hero.js
+++ src/components/Home/Hero/Hero.js
@@ -1,13 +1,33 @@
-import React from 'react'
-import cover from '../../../assets/bg-cover.jpg'
+import React, { useState, useEffect } from 'react'
+import coverMobile from '../../../assets/bg-cover-mobile.jpg'
+import coverDesktop from '../../../assets/bg-cover.jpg'
 import './styles.css'
 
 function Hero() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    useEffect(() => {
+        // Update the windowWidth state when the window is resized
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        // Clean up the event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    // Determine which image to display based on screen width
+    const backgroundImage = windowWidth <= 600 ? coverMobile : coverDesktop;
+
     return (
         <div className='Hero'>
-            <img src={cover} width="100%" />
+            <img src={backgroundImage} width="100%" alt="Hero Background" />
             <div className='title-name'>
-                <h1>ICEBRAKER BARTENDING</h1>
+                <h1>ICEBREAKER BARTENDING</h1>
                 <h1>WE MIX. YOU MINGLE.</h1>
             </div>
         </div>
